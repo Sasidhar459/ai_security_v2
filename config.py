99ac9b02e9eacc8644388ab855f18656bb48d989
Sasidhar459 <131366574+Sasidhar459@users.noreply.github.com>
2026-05-10 09:22:23 +0530

    Updated config.py
    Replaced sensitive Telegram bot token and chat ID with placeholders.

--- config.py
+++ config.py
@@ -167,8 +167,8 @@ TRACKING_MAX_DISTANCE = 60
 # -------------------------
 # Telegram
 # -------------------------
-TELEGRAM_BOT_TOKEN = "8489650776:AAE8lV1AtvqzXz7L-X8u6kquk0Cuj5sOtM8"
-TELEGRAM_CHAT_ID = "6034901248"
+TELEGRAM_BOT_TOKEN = "*****Your telegram bot token*****"
+TELEGRAM_CHAT_ID = "*****Your tlegram chat id*****"
 
 # -------------------------
 # Optional
